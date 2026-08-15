a4a3f9564b72de8ea736be55e700d3342ac658a4
GitHub Action <action@github.com>
2026-08-15 21:33:53 +0000

    chore: bump version to 0.0.7

--- ot-watchdog.php
+++ ot-watchdog.php
@@ -3,7 +3,7 @@
  * Plugin Name: OT Watchdog
  * Plugin URI: https://github.com/incyi/ot-watchdog
  * Description: Lightweight OT status monitoring plugin (PLC, HMI, Switch, Modem)
- * Version: 0.0.5
+ * Version: 0.0.7
  * Author: İnanç Yiğit
  * Author URI: https://github.com/incyi
  */
@@ -15,7 +15,7 @@ defined('ABSPATH') or die('No script kiddies please!');
  * CONFIG
  * =========================
  */
-define('OT_WATCHDOG_VERSION', '0.0.5');
+define('OT_WATCHDOG_VERSION', '0.0.7');
 define('OT_OPTION_KEY', 'ot_watchdog_status');
 
 /**
