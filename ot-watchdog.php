59f660ba38b2870d2846eb00d4da11150ec84e9c
İnanç Yiğit <incyi@users.noreply.github.com>
2026-08-15 23:33:45 +0200

    fix: add Version header to plugin file for WordPress to display version

--- ot-watchdog.php
+++ ot-watchdog.php
@@ -1,7 +1,11 @@
 <?php
 /**
  * Plugin Name: OT Watchdog
+ * Plugin URI: https://github.com/incyi/ot-watchdog
  * Description: Lightweight OT status monitoring plugin (PLC, HMI, Switch, Modem)
+ * Version: 0.0.5
+ * Author: İnanç Yiğit
+ * Author URI: https://github.com/incyi
  */
 
 defined('ABSPATH') or die('No script kiddies please!');
@@ -11,7 +15,7 @@ defined('ABSPATH') or die('No script kiddies please!');
  * CONFIG
  * =========================
  */
-define('OT_WATCHDOG_VERSION', '0.0.6');
+define('OT_WATCHDOG_VERSION', '0.0.5');
 define('OT_OPTION_KEY', 'ot_watchdog_status');
 
 /**
